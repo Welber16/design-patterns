d4acdf2c7148e009b289d7e72acd5ab6f0cc7229
welber16 <binho.3w@gmail.com>
2023-03-28 19:48:12 -0300

    Design-patterms

---
+++ src/TestDeImpostos.java
@@ -0,0 +1,15 @@
+
+public class TestDeImpostos {
+    public static void main(String[] args) {
+
+        Imposto iss = new ISS();
+        Imposto icms = new ICMS();
+
+        Orcamento orcamento = new Orcamento(500.0);
+
+        CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+
+        calculador.realizaCalculo(orcamento, iss);
+        calculador.realizaCalculo(orcamento, icms);
+    }
+}
